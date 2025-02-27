93619ffd2f365b206f0a49f6e95c941ce89205af
elevy30 <elevy30@gmail.com>
2025-02-27 17:43:34 +0200

    git-action first app

---
+++ git-action/test_app.py
@@ -0,0 +1,10 @@
+from app import app
+import unittest
+
+class TestApp(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get("/")
+
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data,"Hello World".encode())
